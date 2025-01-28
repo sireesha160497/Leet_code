04e87c07410962f7542494557c6359028b93c89b
sireesha160497 <sireesha@umich.edu>
2025-01-28 17:36:14 -0500

    SortedSquares

--- Java/src/Main.java
+++ Java/src/Main.java
@@ -6,5 +6,16 @@ public class Main {
         int[] c = {123,546,3333,123456,1};
         Find_number_with_Even_digits f = new Find_number_with_Even_digits();
         System.out.println(f.findNumbers(c));
+        int[] c2 = {13,-16,0,56,-45};
+        SortedSquares sq= new SortedSquares();
+        c2 = sq.sortedSquares(c2);
+        System.out.print("["+c2[0]);
+        for (int i=1;i<c2.length;i++){
+            System.out.print(",");
+            System.out.print(c2[i]);
+
+        }
+        System.out.print("]");
+        System.out.println();
     }
 }
